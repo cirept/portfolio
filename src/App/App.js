a62a65f9e0e3e018c3d222eac12ec114a462e165
Eric Tanaka <cirept612@gmail.com>
2018-07-23 20:38:22 -0700

    added font awesome icons to depedencies

--- src/App/App.js
+++ src/App/App.js
@@ -5,6 +5,15 @@ import logo from '../logo.svg';
 import './App.css';
 
 import NavBar from '../NavBar/NavBar';
+import Jumbotron from '../Components/Jumbotron/Jumbotron';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { faCircle } from '@fortawesome/free-solid-svg-icons'
+
+library.add(far, fab, faCircle);
 
 class App extends Component {
 
@@ -15,46 +24,110 @@ componentWillMount(){
   render() {
     return (
       <div className="App container-fluid">
+
         <header className="App-header container-fluid">
           <NavBar />
         </header>
-        <main>
-          <div className="row align-items-center">
-            <div className="col-sm-4">
-              <img className="img-fluid" src="http://via.placeholder.com/500x500"/>
-            </div>
-            <div className="col-sm-8">
-              <h1>Hello there, I am Eric Tanaka.</h1>
-              <p>I like to code.</p>
+
+        <main className="row">
+          <div className="col-sm-1"></div>
+          <div className="col">
+            <div className="row">
+
+              <section>
+                <div className="row align-items-center">
+                <div className="col-sm-4">
+                  <img className="img-fluid" src="http://via.placeholder.com/500x500"/>
+                </div>
+                <div className="col-sm-8">
+                  <Jumbotron classes={"my-auto"} h1={"Hello there, I'm Eric Tanaka.  A Web Developer.  =]"} leadMessage={"Highly technical and a team player with a passion for taking over the world with JavaScript."} message={"Prototyped numerous workflow tools that have resulted in workflow changes saving 100+ work hours per month for company staff.  Continuously learning and applying new technologies to current flows for productivity and accuracy."} />
+                  Favorite Food: <FontAwesomeIcon icon="stroopwafel" />
+                </div>
+              </div>
+              </section>
+
+              <section className="container-fluid bg-dark">
+                <div className="row">
+                  <div className="col text-light">JS</div>
+                  <div className="col text-light">jQuery</div>
+                  <div className="col text-light">HTML</div>
+                  <div className="col text-light">CSS</div>
+                  <div className="col text-light">Photoshop</div>
+                  <div className="col text-light">React</div>
+                  <div className="col text-light">Git</div>
+                </div>
+                <div className="row">
+                  <div className="col text-light">
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon={['far', 'circle']} className="text-light"/>
+                  </div>
+                  <div className="col text-light">                    
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon={['far', 'circle']} className="text-light"/>
+                  </div>
+                  <div className="col text-light">                    
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon={['far', 'circle']} className="text-light"/>
+                  </div>
+                  <div className="col text-light">                    
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon={['far', 'circle']} className="text-light"/>
+                  </div>
+                  <div className="col text-light">                    
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon={['far', 'circle']} className="text-light"/>
+                    <FontAwesomeIcon icon={['far', 'circle']} className="text-light"/>
+                  </div>
+                  <div className="col text-light">                    
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon={['far', 'circle']} className="text-light"/>
+                    <FontAwesomeIcon icon={['far', 'circle']} className="text-light"/>
+                    <FontAwesomeIcon icon={['far', 'circle']} className="text-light"/>
+                  </div>
+                  <div className="col text-light">                    
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon="circle" className="text-light"/>
+                    <FontAwesomeIcon icon={['far', 'circle']} className="text-light"/>
+                    <FontAwesomeIcon icon={['far', 'circle']} className="text-light"/>
+                    <FontAwesomeIcon icon={['far', 'circle']} className="text-light"/>
+                  </div>
+                </div>
+              </section>
+              
             </div>
           </div>
-          <section className="container-fluid bg-dark">
-            <div className="row">
-              <div className="col text-light">JS</div>
-              <div className="col text-light">jQuery</div>
-              <div className="col text-light">HTML</div>
-              <div className="col text-light">CSS</div>
-              <div className="col text-light">Photoshop</div>
-              <div className="col text-light">React</div>
-              <div className="col text-light">Git</div>
-            </div>
-            <div className="row">
-              <div className="col text-light">JS</div>
-              <div className="col text-light">jQuery</div>
-              <div className="col text-light">HTML</div>
-              <div className="col text-light">CSS</div>
-              <div className="col text-light">Photoshop</div>
-              <div className="col text-light">React</div>
-              <div className="col text-light">Git</div>
-            </div>
-          </section>
+          <div className="col-sm-1"></div>
         </main>
+
         <footer className="container-fluid fixed-bottom text-light bg-dark">
           My Footer
         </footer>
+
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+          // <div className="col"></div>
+          // <div className="col-sm-8">
+
+          //  <div className="col"></div>
---
+++ src/Components/Jumbotron/Jumbotron.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Jumbotron, Button } from 'reactstrap';
+
+const Example = (props) => {
+  return (
+    <div>
+      <Jumbotron className={props.classes}>
+        <h1 className="display-3">{props.h1}</h1>
+        <p className="lead">{props.leadMessage}</p>
+        <hr className="my-2" />
+        <p>{props.message}</p>
+        {props.includeButton && <p className="lead">
+          <Button color="primary">{props.ctaText}</Button>
+        </p>}
+      </Jumbotron>
+    </div>
+  );
+};
+
+export default Example;
